15c42ffee36b3b5ffac656ef1a1528278f1d906e
Robert Montgomery <boobyvxvbooby_gmail.com>
2024-07-24 09:01:22 -0500

    created two non abstract classes car and motorcycle to inherit parent class vehicle

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -79,13 +79,7 @@ namespace ConsoleUI
             
 
             #region Vehicles
-            /* DONE
-             * Create an abstract class called Vehicle
-             * The vehicle class shall have three string properties: Year, Make, and Model
-             * Set the defaults of the properties to something generic in the Vehicle class
-             * Vehicle class shall have an abstract method called DriveAbstract with no implementation.
-             * Vehicle class shall have a virtual method called DriveVirtual with a base implementation.
-             */
+            
 
             /* DONE
              * Now create 2 non-abstract classes: Car and Motorcycle, that inherit from Vehicle
